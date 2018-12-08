771b6c35ae02dd14f21ccab5f4472f837c8dc94d
Curtis S <curtis_scott@live.co.uk>
2018-12-08 18:09:50 +0000

    added more detail

--- app.py
+++ app.py
@@ -4,13 +4,30 @@ from flask_bcrypt import check_password_hash
 from flask_login import (LoginManager, login_user, logout_user,
                              login_required, current_user)
 
+import geocoder,requests
+import googleapiclient
+
+
+
+
 app = Flask(__name__)
 
-
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+FLASK_DEBUG=1
 @app.route('/')
 def hello_world():
+    g = geocoder.ip('me')
+    print(g.city)
+    """r = r = requests.get('127.0.0.1:5000/api/v1/todos')
+    print(r)"""
+    test = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI')
+
+
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host=HOST, port=80)
+
--- models.py
+++ models.py
@@ -4,3 +4,15 @@ from flask_bcrypt import generate_password_hash
 from flask_login import UserMixin
 from peewee import *
 
+DATABASE = SqliteDatabase('social.db')
+
+class User(UserMixin,Model):
+    username = CharField(unique=True)
+
+
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
