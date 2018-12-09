713091f079e263899e8667b722e9427048f96673
Curtis S <curtis_scott@live.co.uk>
2018-12-09 02:25:38 +0000

    more changes added

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask_bcrypt import check_password_hash
 from flask_login import (LoginManager, login_user, logout_user,
                              login_required, current_user)
 
-import geocoder,requests
+import geocoder,requests,json
 import googleapiclient
-
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 
 
 
@@ -16,13 +18,38 @@ DEBUG = True
 PORT = 8000
 HOST = '0.0.0.0'
 FLASK_DEBUG=1
+te = "hello more "
+tert = " hello {}".format(te)
 @app.route('/')
 def hello_world():
     g = geocoder.ip('me')
-    print(g.city)
+    print(g.latlng)
     """r = r = requests.get('127.0.0.1:5000/api/v1/todos')
     print(r)"""
-    test = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI')
+    test = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI')
+    print(json.dumps(test.json()))
+    print(tert)
+
+    test2 = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Banks&circle:200@{},{}&feilds=formatted_address,name&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI'.format(g.latlng[0], g.latlng[1]))
+    test3 = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius=1500&feilds=formatted_address,name&type=shop&keyword=food&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI'.format(g.latlng[0], g.latlng[1]))
+    test4 = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Waitrose&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:1500@{},{}&key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI'.format(g.latlng[0], g.latlng[1]))
+    res = test3.json()
+    print(test3.json())
+    testArr = []
+    for results in res['results']:
+        for info in results:
+            if info == 'name' or info:
+                testArr.append(results[info])
+
+    for shop in testArr:
+        test4 = requests.get(
+            'https://maps.googleapis.com/maps/api/place/findplacefromtext/'
+            'json?input={}&inputtype=textquery&fields=photos,formatted_'
+            'address,name,opening_hours,rating&locationbias=circle:1500@{},{}&'
+            'key=AIzaSyDxNK_Fu4JuEcP6Elc1v28nZmteG64nDyI'.format(shop,
+                g.latlng[0], g.latlng[1]))
+
+    
 
 
     return 'Hello World!'
